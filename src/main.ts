4f0650dd2a35b1551a1fc01c2ddb12323f85a079
Roy Palomino <roy100995@gmail.com>
2022-03-07 00:14:04 -0500

    refactor: import function moved

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,13 @@
 import plays from "./data/plays.json";
 import invoices from "./data/invoices.json";
-import { Invoice, Performance, Play, EnrichedPerformance } from "./types";
+import createStatementData from "./createStatementdata";
+import { Invoice } from "./types";
 
 export function statement(invoice: Invoice, plays: any) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
-export function createStatementData(invoice: Invoice, plays: any) {
-  const statementData = {} as any;
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-
-  function enrichPerformance(aPerformance: Performance) {
-    const result: any = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance: Performance): Play {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance: EnrichedPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`Unknown type: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance: EnrichedPerformance): number {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" == aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function totalVolumeCredits(data: any) {
-    return data.performances
-      .reduce((total: any, p: any) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmount(data: any) {
-    return data.performances
-      .reduce((total: number, p: any) => total + p.amount, 0);
-  }
-}
-
-export function renderPlainText(data: any) {
+function renderPlainText(data: any) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
